302853c1ccc11ad4df99c82838e184a1a82f9aae
JooHwan-Lee <18079916d@connect.polyu.hk>
2023-09-12 15:55:28 +0800

    Initial Commit

---
+++ MobilePayment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MobilePayment
+//
+//  Created by 이주환 on 2023/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobilePayment/MobilePaymentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilePaymentApp.swift
+//  MobilePayment
+//
+//  Created by 이주환 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct MobilePaymentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
